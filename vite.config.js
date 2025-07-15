ed6b42e29828bb94092f4c83943feb7e881f18b3
Julien CAPUL <Julien.CAPUL@cea.fr>
2025-07-15 15:57:14 +0200

    update to push dist/ folder with build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./Ultim8/",
+	base: "https://jcapul.github.io/Ultim8",
 	plugins: [preact()],
 });
