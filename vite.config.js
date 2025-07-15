1f1a3c9133b8388dc3ef424e7a644652a9a00bbc
Julien CAPUL <Julien.CAPUL@cea.fr>
2025-07-15 16:15:47 +0200

    update config to make the build in GH

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "https://jcapul.github.io/Ultim8",
+	base: "/Ultim8/",
 	plugins: [preact()],
 });
